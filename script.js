23b51f50bc8f5e78472124d4da24959e52b0e453
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2024-03-14 21:24:36 +0000

    Changeover

---
+++ script.js
@@ -0,0 +1,21 @@
+const techStackElem = document.getElementById('tech');
+
+if(window.screen.width >= 768){
+    techStackElem.addEventListener('mouseenter', () => {
+        const [stackA, stackB] = techStackElem.children
+        stackA.children[0].textContent = 'techIKnow='
+        stackA.classList.add('stack-a__col')
+        stackB.classList.add('stack-b__col')
+    })
+    techStackElem.addEventListener('mouseleave', () => {
+        const [stackA, stackB] = techStackElem.children
+        stackA.children[0].textContent = 'XXXXXXXXXX'
+        stackA.classList.remove('stack-a__col')
+        stackB.classList.remove('stack-b__col')
+    })
+} else {
+    const [stackA, stackB] = techStackElem.children
+    stackA.children[0].textContent = 'techIKnow='
+    stackA.classList.add('stack-a__col')
+    stackB.classList.add('stack-b__col')
+}
